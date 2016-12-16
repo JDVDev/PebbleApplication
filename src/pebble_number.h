1b2566043acf28d7e6d841ac3819b1ce05d8185f
Owantjaaaa <owen.hoogenboezem@student.hu.nl>
2016-12-16 15:25:04 +0100

    unused

--- src/pebble_number.h
+++
@@ -1,9 +0,0 @@
-#ifndef PEBBLENUMBER_H
-#define PEBBLENUMBER_H
-
-#include "pebble.h"
-
-void draw_pebble_number(Window * window);
-void update_pebble_number(char * message);
-void hide_pebble_number(bool on_off);
-#endif 
--- src/storage.h
+++
@@ -1,9 +0,0 @@
-#ifndef STORAGE_H
-#define STORAGE_H
-
-#include <pebble.h>
-
-void write_pebble_key_to_storage(int pebble_number);
-int read_pebble_key_from_storage();
-
-#endif
